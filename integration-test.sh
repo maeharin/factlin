675c61515ea1edda9f9045859471b5f2b665b7e6
maeharin <maeharin@gmail.com>
2018-07-28 09:53:07 +0900

    oops

--- integration-test.sh
+++ integration-test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-./gradlew build && (cd ./sample-postgres/ && ./gradlew --stacktrace factlinGen && ./gradlew test)
-./gradlew build && (cd ./sample-mariadb/ && ./gradlew --stacktrace factlinGen && ./gradlew test)
+./gradlew build \
+    && (cd ./sample-postgres/ && ./gradlew --stacktrace factlinGen && ./gradlew test) \
+    && (cd ./sample-mariadb/ && ./gradlew --stacktrace factlinGen && ./gradlew test)
 
