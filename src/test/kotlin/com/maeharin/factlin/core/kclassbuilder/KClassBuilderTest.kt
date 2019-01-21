fa648f5280ef227a5f08839dfd9b185ac66a9a8d
suusan2go <ksuzuki180@gmail.com>
2019-01-21 15:26:53 +0900

    add option for camel case  property name

--- src/test/kotlin/com/maeharin/factlin/core/kclassbuilder/KClassBuilderTest.kt
+++ src/test/kotlin/com/maeharin/factlin/core/kclassbuilder/KClassBuilderTest.kt
@@ -33,7 +33,7 @@ class KClassBuilderTest {
     @Test
     fun test_build_with_postgres_dialect() {
 
-        val kClass = KClassBuilder(table, Dialect.POSTGRES, emptyList(), emptyMap()).build()
+        val kClass = KClassBuilder(table, Dialect.POSTGRES, emptyList(), emptyMap(), false).build()
 
         assertAll(
                 "assert proper kClass",
@@ -55,7 +55,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(true, p.isPrimaryKey)
                     assertEquals("primary key", p.comment)
-                    assertEquals("id", p.name())
+                    assertEquals("id", p.name)
                     assertEquals("0", p.defaultValue())
                 },
                 {
@@ -65,7 +65,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("name long long comment", p.comment)
-                    assertEquals("name", p.name())
+                    assertEquals("name", p.name)
                     assertEquals("\"\"", p.defaultValue())
                 },
                 {
@@ -75,7 +75,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("job name", p.comment)
-                    assertEquals("job", p.name())
+                    assertEquals("job", p.name)
                     // todo enable to "engineer"
                     assertEquals("\"\"", p.defaultValue())
                 },
@@ -86,7 +86,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("activate status", p.comment)
-                    assertEquals("status", p.name())
+                    assertEquals("status", p.name)
                     // todo enable to "ACTIVE"
                     assertEquals("\"\"", p.defaultValue())
                 },
@@ -97,7 +97,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("age", p.comment)
-                    assertEquals("age", p.name())
+                    assertEquals("age", p.name)
                     // todo enable to 30
                     assertEquals("0", p.defaultValue())
                 },
@@ -108,7 +108,7 @@ class KClassBuilderTest {
                     assertEquals(true, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("nick name", p.comment)
-                    assertEquals("nick_name", p.name())
+                    assertEquals("nick_name", p.name)
                     assertEquals("null", p.defaultValue())
                 },
                 {
@@ -118,7 +118,7 @@ class KClassBuilderTest {
                     assertEquals(false, p.isNullable)
                     assertEquals(false, p.isPrimaryKey)
                     assertEquals("my custom type column", p.comment)
-                    assertEquals("my_custom_type_col", p.name())
+                    assertEquals("my_custom_type_col", p.name)
                     assertEquals("\"\"", p.defaultValue())
                 }
         )
@@ -131,7 +131,7 @@ class KClassBuilderTest {
                 listOf("users", "status", "\"ACTIVE\""),
                 listOf("users", "age", "20")
         )
-        val kClass = KClassBuilder(table, Dialect.POSTGRES, customDefaultValues, emptyMap()).build()
+        val kClass = KClassBuilder(table, Dialect.POSTGRES, customDefaultValues, emptyMap(), false).build()
 
         assertAll(
                 {
@@ -150,11 +150,54 @@ class KClassBuilderTest {
     }
 
     @Test
+    fun test_build_with_use_camel_case() {
+        val kClass = KClassBuilder(table, Dialect.POSTGRES, emptyList(), emptyMap(), true).build()
+
+        assertAll(
+            {
+                val p = kClass.props[0]
+                assertEquals("id", p.columnName)
+                assertEquals("id", p.name)
+            },
+            {
+                val p = kClass.props[1]
+                assertEquals("name", p.columnName)
+                assertEquals("name", p.name)
+            },
+            {
+                val p = kClass.props[2]
+                assertEquals("job", p.columnName)
+                assertEquals("job", p.name)
+            },
+            {
+                val p = kClass.props[3]
+                assertEquals("status", p.columnName)
+                assertEquals("status", p.name)
+            },
+            {
+                val p = kClass.props[4]
+                assertEquals("age", p.columnName)
+                assertEquals("age", p.name)
+            },
+            {
+                val p = kClass.props[5]
+                assertEquals("nick_name", p.columnName)
+                assertEquals("nickName", p.name)
+            },
+            {
+                val p = kClass.props[6]
+                assertEquals("my_custom_type_col", p.columnName)
+                assertEquals("myCustomTypeCol", p.name)
+            }
+        )
+    }
+
+    @Test
     fun test_build_with_custom_type_mapper() {
         val customTypeMapper = mapOf<String, String>(
                 "my_custom_type" to "STRING"
         )
-        val kClass = KClassBuilder(table, Dialect.POSTGRES, emptyList(), customTypeMapper).build()
+        val kClass = KClassBuilder(table, Dialect.POSTGRES, emptyList(), customTypeMapper, false).build()
 
         assertAll(
                 {
